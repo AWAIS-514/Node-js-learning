7208b1950c17d8bf9e6bd98d14de329959cc97c0
Hassan <meet2hassan02@gmail.com>
2021-08-06 21:38:13 -0700

    8/6/2021
    8/6/2021

---
+++ impExp/imp.js
@@ -0,0 +1,53 @@
+
+
+let add=(a,b)=>{
+console.log("1.Add is working :",a+b);
+
+}
+
+
+
+let Subtract=(a,b)=>{
+    console.log("1.Subtract is working :",a-b);
+    
+    }
+
+
+
+
+    let Multiply=(a,b)=>{
+        console.log("1.Mutiply is working :",a*b);
+        
+        }
+
+
+
+        let Divide=(a,b)=>{
+            console.log("1.Divide is working :",a/b);
+            
+            }
+
+
+
+
+
+            const name='Hassan_Hmc'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+module.exports={add,Subtract,Divide,Multiply,name}
